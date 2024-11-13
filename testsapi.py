a9246e3b3a34969b1f1d190f876c081ae8534bfd
timorvan <titouan_morvan@ensae.fr>
2024-11-13 08:44:56 +0000

    testapi1

---
+++ testsapi.py
@@ -0,0 +1,7 @@
+import requests
+
+url = "https://api.myanimelist.net/v2/anime/30230?fields=id,title,main_picture,alternative_titles,start_date,end_date,synopsis,mean,rank,popularity,num_list_users,num_scoring_users,nsfw,created_at,updated_at,media_type,status,genres,my_list_status,num_episodes,start_season,broadcast,source,average_episode_duration,rating,pictures,background,related_anime,related_manga,recommendations,studios,statistics"
+client_ID = {'X-MAL-CLIENT-ID': 'c2db532c391bf31339ffd6afa650d528'}
+
+
+
