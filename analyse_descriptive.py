0802904f14148d31d2d619e1365c471efa0e6628
fjenft <felix.jenft@ensae.fr>
2024-12-10 22:53:16 +0000

    correction

--- analyse_descriptive.py
+++ analyse_descriptive.py
@@ -7,4 +7,5 @@ print(anime_data['mean'].describe())
 #Comme une moyenne du score des utilisateurs pour un anime donné était mise à 0 quand elle était inconnue (NaN), on va supprimer les animes dont la moyenne est 0.
 #De manière intuitive, il est normal de supprimer ces animes car une moyenne de scores par les utilisateurs de 0 est quasi impossible
 
-print(anime_data[anime_data['mean'] !=0]['mean'].describe())
+anime_data_for_score=anime_data[anime_data['mean'] !=0]
+print(anime_data_for_score['mean'].describe())
