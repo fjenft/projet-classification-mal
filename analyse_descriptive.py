1c70565f37aa27fe749f23fd3506a4c3144c998c
fjenft <felix.jenft@ensae.fr>
2024-12-10 16:53:00 +0000

    début analyse descriptive

---
+++ analyse_descriptive.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+anime_data=pd.read_csv("anime_data.csv")
+
+print(anime_data['mean'].describe())
+
+#Comme une moyenne du score des utilisateurs pour un anime donné était mise à 0 quand elle était inconnue (NaN), on va supprimer les animes dont la moyenne est 0.
+#De manière intuitive, il est normal de supprimer ces animes car une moyenne de scores par les utilisateurs de 0 est quasi impossible
+
+print(anime_data[anime_data['mean'] !=0]['mean'].describe())
