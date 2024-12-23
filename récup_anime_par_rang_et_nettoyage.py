3eadcf8f4fc0ea097f2ebf99810681f0e7f5510d
fjenft <felix.jenft@ensae.fr>
2024-12-23 16:16:06 +0000

    modif commentaires sur le code

--- récup_anime_par_rang_et_nettoyage.py
+++ récup_anime_par_rang_et_nettoyage.py
@@ -2,20 +2,20 @@ import requests
 import pandas as pd
 import ast
 
-#comme l'API ne nous permet pas de récupérer les animes en groupe à travers leur ID, on fait le faire par le rang décroissant (en prenant les 100 1ers rangs, ainsi de suite...)
+#comme l'API ne nous permet pas de récupérer les animes en groupe à travers leur ID, on récupère la liste des animés et leur caractéristiques à partir de leur le rang, dans l'ordre décroissant décroissant (en prenant les 100 1ers rangs, puis les 100 rangs suivants ainsi de suite...)
 
-all_anime = []  # List to store all anime data
-nbr_needed = 27490  # Total number of anime on MAL as of 13/11/2024 (obtained by looking directly on the site of myanimelist)
+all_anime = [] 
+nbr_needed = 27490  #Total number of anime on MAL as of 13/11/2024 (obtained by looking directly on the site of myanimelist)
 
 ID = {'X-MAL-CLIENT-ID': 'c2db532c391bf31339ffd6afa650d528'}
 url = 'https://api.myanimelist.net/v2/anime/ranking'
 parameters = {
-    'ranking_type': 'all',  # Retrieve anime across all rankings
+    'ranking_type': 'all',  
     'limit': 100,  # Max limit per request, divides the total number of anime on mal
     'fields': 'id,title,mean,start_date,end_date,rank,popularity,num_list_users,num_scoring_users,nsfw,media_type,status,num_episodes,start_season,broadcast,source,average_episode_duration,rating'
 }
 
-k = 0  # offset but also the number of times the loop is used that is 27490/127 here
+k = 0  # offset but also the number of times the loop is used that is 27490/100 here
 
 # Loop until we've collected the target number of anime
 while k < nbr_needed:
@@ -25,14 +25,11 @@ while k < nbr_needed:
     
     if mal.status_code == 200: # Check if the request is successful
         data = mal.json()
-        # add as much new anime as number in limits
         all_anime.extend(data['data'])
         k += parameters['limit']
 
-        # Print progress
         print(str(len(all_anime)) + " collected for the moment...")
     
-        # When we reach the target number, stops
         if len(all_anime) >= nbr_needed:
             print("the total number of anime collected is " + str(len(all_anime)))
             break
@@ -40,13 +37,13 @@ while k < nbr_needed:
         print("cannot retrieve more than " + str(len(all_anime))) 
         break
 
-anime_data = pd.DataFrame(all_anime) #final data put in dataframe
+anime_data = pd.DataFrame(all_anime) 
 print(anime_data.head(2))
 
 print(anime_data.head())
 print(anime_data.info())
 
-#On voit que toutes les caractéristiques de chaque anime est contenu dans un "node", un dictionnaire, sauf le rang. 
+#On voit que le dataframe est constitué du rang des animé et d'un "node", un dictionnaire qui contient toutes les caractéristiques de chaque anime.
 #Il faut donc extraire chaque élément du dictionnaire node pour en faire des colonnes à part entière
 
 # On extrait toutes les clés du dictionnaire 'node' et on les transforme en colonnes du dataframe
@@ -79,8 +76,7 @@ for i in anime_data.columns:
     k = anime_data[i].isna().sum()
     print(f"Le nombre de NaN dans la colonne '{i}' est : {k}")
 
-#on gère les différents types de NaN
-
+#On gère les différents types de NaN
 anime_data['source'] = anime_data['source'].fillna('source_inconnue')
 anime_data['rating'] = anime_data['source'].fillna('rating_inconnu')
 anime_data['mean'] = anime_data['mean'].fillna(0)
@@ -102,7 +98,7 @@ anime_data_num = anime_data.select_dtypes(include=["number"])
 # Calcul de la matrice de corrélation
 print(anime_data_num.corr())
 
-# Sauvegarder le DataFrame en fichier CSV local
+#On sauvegarde le DataFrame en fichier CSV local
 local_file_path = "anime_data.csv"
 anime_data.to_csv(local_file_path, index=False)
 print(f"Fichier CSV sauvegardé localement : {local_file_path}")
